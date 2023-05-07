6466f3bd4d3ef8a299807e86ad0e92d783c9094f
Anton <60780638+antonio63963@users.noreply.github.com>
2023-05-07 16:50:40 +0300

    created NoteDetailsContainer

---
+++ src/containers/NoteDetailsContainer/NoteDetailesContainer.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { NoteDetails } from "../../components";
+
+const NoteDetailsContainer: FC = () => {
+  return (
+    <>
+      <NoteDetails />
+    </>
+  );
+};
+
+export default NoteDetailsContainer;
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,4 +1,4 @@
 export {default as HeaderContainer} from './HeaderContainer/HeaderContainer';
 export {default as ContentContainer} from './ContentContainer/ContentContainer';
-export {default as NoteDetailesContainer} from './NoteDetailesContainer/NoteDetailesContainer';
+export {default as NoteDetailesContainer} from './NoteDetailsContainer/NoteDetailesContainer';
 // export {default as NotesListContainer} from './NotesListContainer/NotesListContainer';
