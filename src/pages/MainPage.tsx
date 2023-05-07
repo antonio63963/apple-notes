05ffe73dec558bbd256259881b76fec924484e14
Anton <60780638+antonio63963@users.noreply.github.com>
2023-05-07 16:51:43 +0300

    removed unused dependencies

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,4 @@
 import React, { FC } from "react";
-import cn from "classnames";
-
-import { Sidebar } from "../components";
-import Note from "../components/NoteDetailes/NoteDetailes";
 import { ContentContainer, HeaderContainer } from "../containers";
 
 const MainPage: FC = () => {
