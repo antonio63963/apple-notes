7fec8377da14b7744989ddc3fe20b4dccfe9a20b
Anton <60780638+antonio63963@users.noreply.github.com>
2023-05-09 18:15:00 +0300

    Note component react on select

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -3,8 +3,38 @@ import cn from "classnames";
 
 import styles from "./Note.module.css";
 
-const Note: FC = () => {
-  return <>Note</>;
+interface INote {
+  title: string;
+  time: string;
+  description: string;
+  onClick: () => void;
+  isSelected: boolean;
+}
+
+const Note: FC<INote> = ({
+  title,
+  time,
+  description,
+  onClick,
+  isSelected,
+}) => {
+  console.log(isSelected)
+  return (
+    <div
+      className={cn(styles.container, isSelected && styles.selected)}
+      onClick={onClick}
+    >
+      <div className={cn(styles.content)}>
+        <h3 className={cn(styles.titlej, styles.cropText)}>{title}</h3>
+        <div className={cn(styles.subtitle)}>
+          <span className={cn(styles.time)}>{time} PM</span>
+          <span className={cn(styles.description, styles.cropText)}>
+            {description}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Note;
