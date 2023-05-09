92d8c2841f99c3aad017b68c072d6c534bf3721c
Anton <60780638+antonio63963@users.noreply.github.com>
2023-05-09 12:44:09 +0300

    indexedDB refactoring

--- src/services/indexedDB.ts
+++ src/services/indexedDB.ts
@@ -1,12 +1,3 @@
-interface ILocalDB {
-  openRequest: IDBOpenDBRequest | null;
-  // db: IDBDatabase | null;
-  open: () => {
-    add: (note: INote) => void;
-    delete: () => void;
-    deleteDB: () => void;
-  };
-}
 interface INote {
   id: string;
   date: Date;
@@ -16,40 +7,25 @@ interface INote {
 interface IEventError extends Event {
   target: { error?: string } | any;
 }
+interface IDBMethods {
+  getAll: () => Promise<INote[]>;
+  deleteDB: () => void;
+  deleteStorage: () => void;
+  add: (note: INote) => any;
+  put: (note: INote, id: string) => any;
+  delete: (id: string) => any;
+  clearDB: () => any;
+}
 
-class LocacDB {
-  db: IDBDatabase | null = null;
-  async open() {
-    return new Promise((resolve, reject) => {
-      const openRequest = indexedDB.open("appDB", 1);
-      openRequest.onupgradeneeded = () => {
-        const dbOpen = openRequest.result;
-        if (!dbOpen.objectStoreNames.contains("notes")) {
-          dbOpen.createObjectStore("notes", {
-            keyPath: "id",
-            autoIncrement: true,
-          });
-        }
-      };
-      openRequest.onsuccess = (event: IEventError) => {
-        const db = event.target?.result;
-        resolve({
-          deleteDB: this.deleteDB,
-          add: (note: INote) => this.add(db, note),
-          put: (note: INote, id: string) => this.put(db, note, id),
-          delete: (id: string) => this.delete(db, id),
-          clearDB: () => this.clearDB(db),
-          getAll: () => this.getAll(db),
-        });
-      };
-      openRequest.onerror = () => {
-        reject("Open DB Error!");
-      };
-    });
+async function initLocalDB(
+  storageName: string,
+  dbName: string
+): Promise<IDBMethods> {
+  function deleteStorage(db: IDBDatabase) {
+    db?.deleteObjectStore(dbName);
   }
-
-  deleteDB() {
-    const deleteRequest = indexedDB.deleteDatabase("appDB");
+  function deleteDB() {
+    const deleteRequest = indexedDB.deleteDatabase(storageName);
     deleteRequest.onsuccess = function () {
       console.log("DB has been deleted... ", deleteRequest.result);
     };
@@ -58,19 +34,22 @@ class LocacDB {
     };
   }
 
-  add(db: IDBDatabase, note: INote) {
-    const notes = db?.transaction("notes", "readwrite").objectStore("notes");
-
-    const request = notes.add(note);
-    request.onsuccess = function () {
-      console.log("Note was added in store...", request.result);
-    };
-    request.onerror = function () {
-      console.log("Error", request.error, "---", request);
-    };
+  function add(db: IDBDatabase, note: INote): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const notes = db?.transaction([dbName], "readwrite").objectStore(dbName);
+      const request = notes.add(note);
+      request.onsuccess = function () {
+        console.log("Note was added in store...", request.result);
+        resolve(request.result);
+      };
+      request.onerror = function () {
+        console.log("Error", request.error);
+        reject(`Error!!! Item hasn't added`);
+      };
+    });
   }
-  put(db: IDBDatabase, updatedNote: INote, id: string) {
-    const notes = db?.transaction("notes", "readwrite").objectStore("notes");
+  function put(db: IDBDatabase, updatedNote: INote, id: string) {
+    const notes = db?.transaction(dbName, "readwrite").objectStore(dbName);
     const idRequest = notes.get(id);
 
     idRequest.onsuccess = () => {
@@ -85,8 +64,8 @@ class LocacDB {
       };
     };
   }
-  delete(db: IDBDatabase, id: string) {
-    const notes = db?.transaction("notes", "readwrite").objectStore("notes");
+  function deleteItem(db: IDBDatabase, id: string) {
+    const notes = db?.transaction(dbName, "readwrite").objectStore(dbName);
     const idRequest = notes.get(id);
 
     idRequest.onsuccess = () => {
@@ -100,20 +79,56 @@ class LocacDB {
       };
     };
   }
-  clearDB(db: IDBDatabase) {
-    const notes = db?.transaction("notes", "readwrite").objectStore("notes");
+  function clearDB(db: IDBDatabase) {
+    const notes = db?.transaction(dbName, "readwrite").objectStore(dbName);
     const clearStorage = notes.clear();
     clearStorage.onsuccess = () => {
       console.log("DB has been cleared...", clearStorage.result);
     };
   }
-  getAll(db: IDBDatabase) {
-    const notes = db?.transaction("notes", "readwrite").objectStore("notes");
-    const getAllRequest = notes.getAll();
-    getAllRequest.onsuccess = () => {
-      console.log("Got all items...", getAllRequest.result);
-    };
+  function getAll(db: IDBDatabase): Promise<INote[]> {
+    return new Promise((resolve, reject) => {
+      const notes = db?.transaction(dbName, "readwrite").objectStore(dbName);
+      const getAllRequest = notes.getAll();
+
+      getAllRequest.onsuccess = () => {
+        console.log("Got all items...", getAllRequest.result);
+        resolve(getAllRequest.result);
+      };
+      getAllRequest.onerror = () => {
+        reject("Error... Something has gone wrong...");
+      };
+    });
   }
+
+  return new Promise((resolve, reject) => {
+    const openRequest = indexedDB.open(storageName, 1);
+    openRequest.onupgradeneeded = () => {
+      const dbOpen = openRequest.result;
+      if (!dbOpen.objectStoreNames.contains(dbName)) {
+        dbOpen.createObjectStore(dbName, {
+          keyPath: "id",
+          autoIncrement: true,
+        });
+      }
+    };
+    openRequest.onsuccess = (event: IEventError) => {
+      const db = event.target?.result;
+      resolve({
+        getAll: () => getAll(db),
+        deleteDB,
+        deleteStorage: () => deleteStorage(db),
+        add: (note: INote) => add(db, note),
+        put: (note: INote, id: string) => put(db, note, id),
+        delete: (id: string) => deleteItem(db, id),
+        clearDB: () => clearDB(db),
+      });
+    };
+    openRequest.onerror = () => {
+      reject("Open DB Error!");
+    };
+  });
 }
 
-export default new LocacDB();
+export default initLocalDB;
+export type { INote };
